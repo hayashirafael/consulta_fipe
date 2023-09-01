f5d72862dce8ef4aec144e2038e6881e6ec2737c
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-09-01 00:47:38 -0300

    chore: removendo Modular e aplicando provider via flutter_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:consulta_fipe/src/app_module.dart';
 import 'package:consulta_fipe/src/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'src/firebase_options.dart';
 
 Future<void> main() async {
@@ -11,8 +9,5 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(ModularApp(
-    module: AppModule(),
-    child: const AppWidget(),
-  ));
+  runApp(const AppWidget());
 }
--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,17 +1,24 @@
+import 'package:consulta_fipe/src/features/auth/data/services/firebase_auth_service.dart';
+import 'package:consulta_fipe/src/features/auth/interactor/blocs/auth_bloc.dart';
+import 'package:consulta_fipe/src/features/auth/ui/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Modular.setInitialRoute('/login');
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routerConfig: Modular.routerConfig,
-      // routeInformationParser: Modular.routeInformationParser,
-      // routerDelegate: Modular.routerDelegate,
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+              create: (BuildContext context) => AuthBloc(FirebaseAuthService(FirebaseAuth.instance))),
+        ],
+        child: const LoginPage(),
+      ),
     );
   }
 }
